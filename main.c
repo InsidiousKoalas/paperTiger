b92cb4647b05b960245ffe869e59e501268b8b0b
InsidiousKoalas <49494064+InsidiousKoalas@users.noreply.github.com>
2019-04-29 23:37:48 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include <msp430.h> 
-//#include "ledFunks.h"
-//#include "vuFunks.h"
 
 /*
  * main.c
@@ -8,13 +6,15 @@
 #define NUM_SAMPS	1024
 #define UART_W8		0x3FFF
 
-unsigned int *findFreq(int*, unsigned int);
-int zeroX(int*, unsigned int);
+//unsigned int *findFreq(int*, unsigned int);
+int* zeroX(int*, unsigned int);
+int vuMeter(int*);
+void xFerData(unsigned int*);
 void initClockTo16MHz();
 
 int sample, sampFlag, pushData, ndx;
 unsigned long int avg;
-volatile char eosFlag = 0;
+volatile char sendFlag = 0;
 
 /*
  * P3.3 is TX
@@ -73,61 +73,70 @@ int main(void) {
     P1REN |= BIT1;
     P1OUT |= BIT1;
 
+    // Reset constituent MSP430 display controller
+    P1DIR |= BIT3;
+    P1OUT |= BIT3;
+    __delay_cycles(0xFFFF);
+    P1OUT &= ~BIT3;
+    __delay_cycles(0xFFFF);
+    P1OUT |= BIT3;
+
 
 /*
  * 	-----------------	ADC initialization  ------------------------
  */
 
-    UCSCTL0 = DCO0 | DCO1 | DCO2 | DCO3 | DCO4;
     initClockTo16MHz();
-//    BCSCTL1 = RSEL0 + RSEL1 + RSEL2;
-//    UCSCTL1 =
-//    DCOCTL0 = DCO0 + DCO1 + DCO2;
-    ADC12CTL0 = ADC12SHT02 + ADC12ON;         // Sampling time, ADC12 on
-    ADC12MCTL0 = ADC12INCH_1;					// multiplex ADC to P6.1
-    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_0 | ADC12SSEL_0;                     // Use sampling timer
-    ADC12IE = 0x01;                           // Enable interrupt
-    ADC12CTL0 |= ADC12ENC;					  // Enable encoding
+    ADC12CTL0 = ADC12SHT02 + ADC12ON;           				// Sampling time, ADC12 on
+    ADC12MCTL0 = ADC12INCH_1;									// multiplex ADC to P6.1
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_0 | ADC12SSEL_0;         // Use sampling timer
+    ADC12IE = 0x01;                             				// Enable interrupt
+    ADC12CTL0 |= ADC12ENC;					    				// Enable encoding
 
 
 
     P1DIR |= BIT0;
 
-
-    // test ADC
-//    ADC12CTL0 |= ADC12SC;
-//    __bis_SR_register(GIE);        // LPM0 with interrupts enabled (CPUOFF waits for ADC)
-
 /*
  * 	-----------------	Timer initialization  ------------------------
  */
 
-//    TA0CCTL0 = CCIE;
-    TA0CCR0 = 20;
+    TA0CCR0 = 400;
 
-    unsigned int fs = 864/TA0CCR0*1000;		// tuned to produce accurate notes
+    unsigned int fs = 1600/TA0CCR0*10000+300;		// 	sampling frequency; 16MHz/TA0CCR0 + correction factor
 
     TA0CTL = TASSEL_2 + MC_1;	// SMCLK, upmode
 
 /*
- *  ----------------   UART initialization  --------------------------
+ *  ----------------   UART0 initialization  --------------------------
+ */
+    P3SEL |= BIT3+BIT4;                       // P3.3,4 = USCI_A0 TXD/RXD
+    UCA0CTL1 |= UCSWRST;                      // **Put state machine in reset**
+    UCA0CTL1 |= UCSSEL_2;                     // SMCLK
+    UCA0BR0 = 139;                              // 1MHz 115200 (see User's Guide)
+    UCA0BR1 = 0;                              // 1MHz 115200
+    UCA0MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
+    UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
+    UCA0IE |= UCRXIE;                         // Enable USCI_A0 RX interrupt
+
+
+/*
+ *  ----------------   UART1 initialization  --------------------------
  */
     P4SEL |= BIT4|BIT5;                       // P3.3,4 = USCI_A0 TXD/RXD
     UCA1CTL1 |= UCSWRST;                      // **Put state machine in reset**
     UCA1CTL1 |= UCSSEL_2;                     // SMCLK
-    UCA1BR0 = 139;                              // 16MHz, 115200 (see User's Guide)
+    UCA1BR0 = 139;                            // 16MHz, 115200 (see User's Guide)
     UCA1BR1 = 0;                              // 1MHz 115200
     UCA1MCTL |= UCBRS_1 + UCBRF_0;            // Modulation UCBRSx=1, UCBRFx=0
     UCA1CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-    UCA1IE |= UCRXIE;                         // Enable USCI_A0 TX interrupt
 
 
-    // send init over UART
-    __bis_SR_register(GIE);
+    // send initialization text over UART
 
     UCA1TXBUF = 'o';
     __delay_cycles(UART_W8);
-    UCA1TXBUF = 'n';;
+    UCA1TXBUF = 'n';
     __delay_cycles(UART_W8);
     UCA1TXBUF = '\r';
     __delay_cycles(UART_W8);
@@ -136,7 +145,7 @@ int main(void) {
     UCA1TXBUF = '\r';
     __delay_cycles(UART_W8);
     UCA1TXBUF = '\n';
-    unsigned char tx[4], tx2[5];
+    unsigned char tx[4], tx2[5];	// UART buffers
 
 
 /*
@@ -146,13 +155,12 @@ int main(void) {
     int sampAry[NUM_SAMPS] = {0};
 //    unsigned int avg=0;
     P4DIR |= BIT7;
-
+    P1DIR &= ~BIT2;				   // Slave ready
     TA0CCTL0 = CCIE;
     __bis_SR_register(GIE);        // enable interrupts
 
 
-    unsigned int *streamPtr;
-    int freq;
+    unsigned int *buffData;
 
 
     while(1){
@@ -162,185 +170,74 @@ int main(void) {
 
     		sampAry[ndx] = sample;		// copy sample val into array
     		ndx++;						// index for next sample
+
+    		// if at the end of the array...
     		if(ndx>NUM_SAMPS){
-    			ndx = 0;
-    			//    			streamPtr = findFreq(sampAry, fs);
-    			freq = zeroX(sampAry, fs);
+    			UCA0IE |= UCRXIE;			// enable receive UART interrupt (check to see if slave is ready)
+    			ndx = 0;					// reset ndx to 0
+    			buffData = zeroX(sampAry, fs);		// [freq, mag] = zeroCrossing ( array of samples, sample freq)
 
-    			tx2[4] = (freq%10)+'0';
-    			freq /= 10;
-    			tx2[3] = (freq%10)+'0';
-    			freq /= 10;
-    			tx2[2] = (freq%10)+'0';
-    			freq /= 10;
-    			tx2[1] = (freq%10)+'0';
-//    			freq /= 10;
-//    			tx2[0] = (freq%10)+'0';
+    			// TX data
+    			if(P1IN & BIT2){				// if slave is ready
+    				TA0CCTL0 &= ~CCIE;			// disable timer interrupts
+    				__bic_SR_register(GIE);		// disable global interrupts
 
+    				// duplicate sample array for dual UART transmissions
+    				int temp[2];
+    				for(i=0; i<2; i++){
+    					temp[i] = buffData[i];
+    				}
+
+    				// Send data to salve uC
+    				UCA0IE &= ~UCRXIE;		// clr UART receive interrupt
+    				xFerData(temp);			// send data to slave
+    				UCA0IE |= UCRXIE;		// reenable UART interrupt
+
+    				sendFlag = 0;
+    			}
+
+    			// Transmit frequencies to computer
+    			UCA1IE |= UCRXIE;				// enable UCA1 interrupts
+
+    			// convert ints to chars
+    			tx2[4] = (buffData[0]%10)+'0';
+    			buffData[0] /= 10;
+    			tx2[3] = (buffData[0]%10)+'0';
+    			buffData[0] /= 10;
+    			tx2[2] = (buffData[0]%10)+'0';
+    			buffData[0] /= 10;
+    			tx2[1] = (buffData[0]%10)+'0';
+
+    			// transmit char array
     			for(j=1;j<5;j++){
     				UCA1TXBUF = tx2[j];
     				__delay_cycles(UART_W8);
     			}
+
+    			// display amplitude information
+    			UCA1TXBUF = '\t';			// space out
+    			__delay_cycles(UART_W8);
+
+    			UCA1TXBUF = (buffData[1]&0x00FF) + '0';		// amplitude char
+    			__delay_cycles(UART_W8);
+
+    			// print carriage return, new line
     			UCA1TXBUF = '\r';
     			__delay_cycles(UART_W8);
     			UCA1TXBUF = '\n';
     			__delay_cycles(UART_W8);
 
-
-    			//    			for(i=0; i<64; i++){
-    			//    				for(j=0; j<16; j++){
-    			//    					UCA1TXBUF = streamPtr[i]&0x01+'0';
-    			////    					UCA1TXBUF = 'g';
-    			//    					streamPtr[i] >>= 0x01;
-    			//    					__delay_cycles(UART_W8);
-    			//    					UCA1TXBUF = '\r';
-    			//    					__delay_cycles(UART_W8);
-    			//    					UCA1TXBUF = '\n';
-    			//    					__delay_cycles(UART_W8);
-    			//    				}
-    			//    			}
-
     		}
 
-    		P4OUT ^= BIT7;		// test if running
-
+    		// tidy up
+    		P4OUT ^= BIT7;				// test if running
+    		UCA0IE &= ~UCRXIE;			// disable UART interrupt from uC
     		__bis_SR_register(GIE);		// reenable globals
-    		TA0CCTL0 |= CCIE;
-//    		eosFlag = 1;
-    		sampFlag = 0;	// clear sample flag
+    		TA0CCTL0 |= CCIE;			// reenable timer interrupt
+
+    		sampFlag = 0;				// clear sample flag
     	}
-
-
-
-
-
-
-
-
-    	// TX data
-    	if(!(P1IN & BIT1)){
-    		TA0CCTL0 &= ~CCIE;
-    		__bic_SR_register(GIE);
-    		UCA1IE |= UCRXIE;
-//    		freq = zeroX(sampAry, fs);
-//    		streamPtr = findFreq(sampAry, fs);
-//
-//    		tx2[4] = (freq%10)+'0';
-//    		freq /= 10;
-//    		tx2[3] = (freq%10)+'0';
-//    		freq /= 10;
-//    		tx2[2] = (freq%10)+'0';
-//    		freq /= 10;
-//    		tx2[1] = (freq%10)+'0';
-//    		freq /= 10;
-//    		tx2[0] = (freq%10)+'0';
-//
-//    		for(j=0;j<5;j++){
-//    			UCA1TXBUF = tx2[j];
-//    			__delay_cycles(UART_W8);
-//    		}
-
-//    		 LPF data
-//    		for(i=0; i < NUM_SAMPS; i++){
-////    			if(i>=NUM_SAMPS)(i=0);
-//
-//    			tx[3] = (streamPtr[i]%10)+'0';
-//    			streamPtr[i] /= 10;
-//    			tx[2] = (streamPtr[i]%10)+'0';
-//    			streamPtr[i] /= 10;
-//    			tx[1] = (streamPtr[i]%10)+'0';
-//    			streamPtr[i] /= 10;
-//    			tx[0] = streamPtr[i]+'0';
-//
-//    			//    			if(i>=64)(i=0);
-//
-//    			UCA1TXBUF = tx[0];
-//    			__delay_cycles(UART_W8);
-//    			//    			for(j=0;j<1;j++){
-////    				UCA1TXBUF = tx[j];
-////    				__delay_cycles(UART_W8);
-////    			}
-//
-//
-//    			UCA1TXBUF = '\r';
-//    			__delay_cycles(UART_W8);
-//    			UCA1TXBUF = '\n';
-//    			__delay_cycles(UART_W8);
-//
-//    		}
-
-    		UCA1TXBUF = '\r';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\n';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\r';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\n';
-    		__delay_cycles(UART_W8);
-
-    		for(i=0; i < NUM_SAMPS; i++){
-//    			if(i>=NUM_SAMPS)(i=0);
-
-    			tx[3] = (sampAry[i]%10)+'0';
-    			sampAry[i] /= 10;
-    			tx[2] = (sampAry[i]%10)+'0';
-    			sampAry[i] /= 10;
-    			tx[1] = (sampAry[i]%10)+'0';
-    			sampAry[i] /= 10;
-    			tx[0] = (sampAry[i]%10)+'0';
-
-//    			if(i>=64)(i=0);
-
-        		for(j=0;j<4;j++){
-        			UCA1TXBUF = tx[j];
-        			__delay_cycles(UART_W8);
-        		}
-
-
-    			UCA1TXBUF = '\r';
-    			__delay_cycles(UART_W8);
-    			UCA1TXBUF = '\n';
-    			__delay_cycles(UART_W8);
-
-    		}
-
-    		tx[3] = (freq%10)+'0';
-    		freq /= 10;
-    		tx[2] = (freq%10)+'0';
-    		freq /= 10;
-    		tx[1] = (freq%10)+'0';
-    		freq /= 10;
-    		tx[0] = (freq%10)+'0';
-
-
-    		for(j=0;j<4;j++){
-    			UCA1TXBUF = tx[j];
-    			__delay_cycles(UART_W8);
-    		}
-
-
-    		pushData = 0;
-    		UCA1TXBUF = '\r';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\n';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\r';
-    		__delay_cycles(UART_W8);
-    		UCA1TXBUF = '\n';
-
-//    		for(i=0; i<(8*sizeof(int)); i++){
-//    			UCA1TXBUF = *()
-//    		}
-//
-//    		eosFlag = 0;
-
-    		TA0CCTL0 |= CCIE;
-    		__bis_SR_register(GIE);
-    	}
-
-
     }
-
 
 	return 0;
 }
@@ -369,67 +266,6 @@ __interrupt void Timer_A (void)
 
 }
 
-// Echo back RXed character, confirm TX buffer is ready first
-#pragma vector=USCI_A1_VECTOR
-__interrupt void USCI_A1_ISR(void)
-{
-	switch(__even_in_range(UCA1IV,4))
-	{
-	case 0:break;                             // Vector 0 - no interrupt
-	case 2:                                   // Vector 2 - RXIFG
-		while (!(UCA1IFG&UCTXIFG));             // USCI_A0 TX buffer ready?
-		//    UCA0TXBUF = UCA0RXBUF;                  // TX -> RXed character
-		__bic_SR_register_on_exit(CPUOFF);
-		break;
-	case 4:break;                             // Vector 4 - TXIFG
-	default: break;
-	}
-}
-
-
-
-
-/*
- * XOR Acorr
- *
- */
-unsigned int * findFreq(int * sampAry, unsigned int fs){
-	int i, j, avg = 2056;
-	char pos[NUM_SAMPS];
-	int aCorr[NUM_SAMPS];		// array of positive locations
-
-	for(i=0; i<NUM_SAMPS; i++){
-		if((sampAry[i]-avg)&0x8000)(pos[i] = 0);		// array is negative
-		else(pos[i] = 1);								// array is pos
-	}
-
-	// autocorrelate positives array
-	for(i=0; i<NUM_SAMPS; i++){
-		aCorr[i] = 0;
-		// for 0 to end of overlap, find correlation
-		for(j=0; j<i; j++){
-			aCorr[i] += pos[j]^pos[i];
-		}
-	}
-
-//	int diff[2] = { 0 };
-//
-//	for(i=0; i<NUM_SAMPS; i++){
-//		if(aCorr[i]<diff[0]){
-//			diff[1] = diff[0];
-//			diff[0] = aCorr[i];
-//
-//		}
-//		dist++;
-//	}
-
-
-
-	return aCorr;
-}
-
-
-
 
 /*
  *
@@ -437,19 +273,30 @@ unsigned int * findFreq(int * sampAry, unsigned int fs){
  *
  *
  */
-int zeroX(int *sampAry, unsigned int fs){
-	int avg = 2056, i, j;
+int* zeroX(int *sampAry, unsigned int fs){
+	int avg = 2053, i, j;
 	long int sum=0;
-	int period=0, periodFlg=0, num=12;
+	float avgDist;
+	int periodFlg=0, num=12;
 
 	int stream[NUM_SAMPS], pos[NUM_SAMPS];
+//	int levels[16] = {2592,2938,3141,3285,
+//			3396,3488,3565,3631,
+//			3690,3743,3791,3834,
+//			3874,3946,3978,4008};
 
+	int levels[16] = {2592,2938,3141,3285,
+			3396,3700,3800,3850,
+			3875,3900,3950,3970,
+			3990,4000,4010,4020};
 
 	// LPF
 	pos[0] = 0;
-	int td[7]={0}, idx=-1;
+	int td[8]={0}, idx=-1, max = 0;
 
+	// for each data point in sampAry
 	for(i=1; i<NUM_SAMPS-num; i++){
+		if(sampAry[i]>max)(max=sampAry[i]);		// record max value of array
 
 		//LPF
 		for(j=0; j<num; j++){
@@ -457,7 +304,6 @@ int zeroX(int *sampAry, unsigned int fs){
 		}
 
 		stream[i] = sum/num;
-//		stream[i] = sampAry[i];
 		sum=0;
 		if((stream[i]-avg) & 0x8000)(pos[i] = 0);
 		else(pos[i] = 1);
@@ -465,7 +311,7 @@ int zeroX(int *sampAry, unsigned int fs){
 		if(pos[i-1] == 0 && pos[i] == 1){
 			periodFlg = 1;
 			idx++;
-			if(idx>6){
+			if(idx>7){
 				break;
 			}
 		}
@@ -476,24 +322,107 @@ int zeroX(int *sampAry, unsigned int fs){
 	}
 
 
-	sum=0;
-	for(i=0; i<6; i++){
-		sum += td[i];
+	float freq;
+	int ret[2];
+
+	avgDist=0;
+
+	// Low frequency, average only 3 zero crossings (0-248 Hz)
+	if((td[1] > 170) && ((td[2]) > 170)){
+		for(i=1; i<4; i++){
+			avgDist += td[i];
+		}
+		avgDist = avgDist*0.334;
 	}
-	sum /= 6;
 
+	// Midrange, average 5 zero crossings (249-601 Hz)
+	else if((td[1] > 70) && ((td[2]) > 70)){
+		for(i=1; i<6; i++){
+			avgDist += td[i];
+		}
+		avgDist = avgDist*0.2;
+	}
 
+	// Highest frequencies (Up to ~21 kHz), average 6 zero crossings
+	else{
+		for(i=1; i<7; i++){
+			avgDist += td[i];
+		}
+		avgDist = avgDist*0.1667;
+	}
 
-	int freq;
+	freq = fs/avgDist;
 
-	freq = fs/sum;
+	ret[0] = freq;
 
+	// VU data
+	i=0;
+	while(levels[i] < max){
+		i++;
+		if(i==15){
+			break;
+		}
+	}
 
-	return freq;
+//	// shift up for ASCII table
+//	if(i>9)(i+=7);
+
+	ret[1] = i;
+
+	return ret;
 }
 
 
 
+/*
+ * Send 2 inegers as bytes over UART to LCD display uC.
+ */
+void xFerData(unsigned int* bData){
+	char buffer[4], i, j, tx2[4];
+//	buffData[1] |= 0xFF00;		// set last nibble in array = 1111 for tracking
+	UCA0IE |= UCRXIE;
+//	buffer[0] = (buffData[0]>>8) & 0xFF;
+//	buffer[1] = buffData[0] & 0xFF;
+//	buffer[2] = buffData[1];
+//	buffer[3] = 0xFF;
+
+	tx2[3] = (bData[0]%10)+'0';
+	bData[0] /= 10;
+	tx2[2] = (bData[0]%10)+'0';
+	bData[0] /= 10;
+	tx2[1] = (bData[0]%10)+'0';
+	bData[0] /= 10;
+	tx2[0] = (bData[0]%10)+'0';
+
+	for(j=0;j<4;j++){
+		UCA0TXBUF = tx2[j];
+		__delay_cycles(UART_W8);
+	}
+
+
+	UCA0TXBUF = bData[1]+'0';
+	__delay_cycles(UART_W8);
+
+
+
+//	buffer[0] = 'r';
+//	buffer[1] = 'x';
+//	buffer[2] = '?';
+//	buffer[3] = '\0';
+//
+//	for(i=0; i<4; i++){
+//		UCA0TXBUF = buffer[i];
+//		__delay_cycles(UART_W8);
+//	}
+	UCA0IE &= ~UCRXIE;
+
+}
+
+
+
+/*
+ * Set SMCLK to 16MHz
+ */
 void initClockTo16MHz()
 {
     UCSCTL3 |= SELREF_2;                      // Set DCO FLL reference = REFO
